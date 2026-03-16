8316d1eb625f0b9dfa816c1a933e1eda7914a255
Hruaia <hruaitluangasailo20@gmail.com>
2026-03-16 06:28:00 +0530

    dik tawh rawh

--- src/pages/SessionRecordings.jsx
+++ src/pages/SessionRecordings.jsx
@@ -126,7 +126,7 @@ export default function SessionRecordings() {
                 <img
                   src={
                     rec.thumbnail_url ||
-                    `https://iframe.mediadelivery.net/thumbnail/${rec.bunny_video_id}.jpg`
+                    `https://vz-615730.b-cdn.net/${rec.bunny_video_id}/thumbnail.jpg`
                   }
                   alt={rec.title}
                 />
